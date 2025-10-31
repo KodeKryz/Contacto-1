9c6f1531093effd10a02f20cae2af6a25c95bcad
KodeKryz <crissaraos87@gmail.com>
2025-10-31 15:09:32 -0300

    Cambios y agregacion de SupeBase en railway

--- ContactosPersonales/settings.py
+++ ContactosPersonales/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'ContactosPersonales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("dbnombre"),
+        "USER": os.getenv("usuario"),
+        "PASSWORD": os.getenv("contraseña"),
+        "HOST": os.getenv("host"),
+        "PORT": os.getenv("puerto"),
     }
 }
 
@@ -123,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRF trusted domains (requerido para Railway)
+CSRF_TRUSTED_ORIGINS = [
+    'https://contacto-1-production.up.railway.app',
+]
+
